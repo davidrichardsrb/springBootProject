b51ec91eb002b35316162563b40b3ee723776cf1
davidrichardsrb <david.richardsrb@outlook.com>
2023-11-09 21:45:40 -0500

    Create project SpringBoot Backed cardatabase

---
+++ carddatabase/src/test/java/com/davecorp/carddatabase/CarddatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davecorp.carddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarddatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
